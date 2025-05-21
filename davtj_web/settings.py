b7fdf9e8dbd37cb655e530591ecf936c0374d7d9
Hamrozjon Kosimov <141737711+Hamroz-JONN@users.noreply.github.com>
2025-05-21 20:15:09 +0500

    add 127.0.0.1 ip

--- davtj_web/settings.py
+++ davtj_web/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['157.245.129.55', 'localhost']
+ALLOWED_HOSTS = ['157.245.129.55', 'localhost', '127.0.0.1']
 
 # Redis setup for Celery
 
